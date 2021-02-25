32238ed4ced9df18ee33866d0988a7493db725a5
Jordan Laor <jordanlaor@gmail.com>
2021-02-25 12:35:47 +0200

    welcome page in the making

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const welcomeBtns = {};
 document.querySelectorAll('.welcome__btn').forEach((btn) => {
   welcomeBtns[btn.id] = btn;
 });
+const pages = {};
+document.querySelectorAll('.page').forEach((page) => {
+  pages[page.id] = page;
+});
 
 function exit() {
   window.close();
@@ -11,6 +15,17 @@ function start() {
   window.location = './game.html';
 }
 
+function switchPage(e) {
+  if (e.currentTarget === welcomeBtns['welcome__btn-explanation']) {
+    pages.welcome.classList.add('hidden');
+    pages.explanation.classList.remove('hidden');
+  } else {
+    pages.welcome.classList.remove('hidden');
+    pages.explanation.classList.add('hidden');
+  }
+}
+
 welcomeBtns['welcome__btn-exit'].addEventListener('click', exit);
 welcomeBtns['welcome__btn-start'].addEventListener('click', start);
-welcomeBtns['welcome__btn-exit'].addEventListener('click', exit);
+welcomeBtns['welcome__btn-explanation'].addEventListener('click', switchPage);
+welcomeBtns['welcome__btn-back'].addEventListener('click', switchPage);
